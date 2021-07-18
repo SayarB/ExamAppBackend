0a6567093f43a6b6f9221a36034f497d25362024
SayarB <sayarbhattacharyya03@gmail.com>
2021-07-18 11:49:40 +0530

    minor routing changes

--- api/routes.js
+++ api/routes.js
@@ -1,46 +1,37 @@
 const express = require("express");
 const Exam = require("../models/Exam");
 const router = express.Router();
-const getExam = (id)=>
-{
-    return {...exam, id: id}
-}
+const getExam = (id) => {
+  return { ...exam, id: id };
+};
 
-router.get("/exam/:id",async (req, res)=>
-{
-    const id = req.params.id
-    const data=await Exam.findById(id)
-    res.status(200).send(data);
-})
-router.post("/exam", (req,res)=>
-{
-    
-    const examData = req.body.exam
-    
-    
-    const exam=new Exam(examData);
-    exam.save().then((data)=>
-    {
-        res.status(200).send(data._id);
-    }).catch(err=>
-        {
-            console.log(err);
-            res.status(500).send("Internal Errors")
-        })
-    
+router.get("/exam/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await Exam.findById(id);
+  res.status(200).send(data);
+});
+router.post("/exam", (req, res) => {
+  const examData = req.body.exam;
 
-})
+  const exam = new Exam(examData);
+  exam
+    .save()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Errors");
+    });
+});
 
-router.get("/exam", async(req,res)=>
-{
-    try{
-    const allExams= await Exam.find({});
+router.get("/exam", async (req, res) => {
+  try {
+    const allExams = await Exam.find({});
+    console.log(allExams);
     res.status(200).json(allExams);
-    }
-    catch(err)
-    {
-        res.status(500).send(err);
-    }
-})
-module.exports=router;
-
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const ExamModel=require("./models/Exam")
+const ExamModel = require("./models/Exam");
 require("dotenv").config();
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 //importing routes
-const apiRoutes = require("./api/routes")
+const apiRoutes = require("./api/routes");
 //Environment Variables
-const MONGO_URI=process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 //DB Connection
-mongoose.connect(MONGO_URI, {useNewUrlParser:true,useUnifiedTopology:true}, ()=>
-{
+mongoose.connect(
+  MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
     console.log("Connected to Database");
-})
+  }
+);
 
 //DB Helper Methods
-
-
-
-
-
-
 
 //routes
 
 app.use("/api/v1", apiRoutes);
 
 //listen on a port
-app.listen(8080, ()=>
-{
-    console.log("Listening on Port 3000");
-})
+app.listen(8080, () => {
+  console.log("Listening on Port 8080");
+});
